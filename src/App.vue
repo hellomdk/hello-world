7fff4b328fc3b8779ea9f59d9529e565b6528c48
hellomdk <670699309@qq.com>
2019-12-13 17:42:45 +0800

    初始demo

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <HelloWorld />
+    <Test msg="我是test哦"/>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+import Test from './test/Test.vue'
+
+export default {
+  name: 'app',
+  components: {
+    HelloWorld,
+    Test
+  }
+}
+
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/test/Test.vue
@@ -0,0 +1,19 @@
+<template>
+    <div id="test">
+        <span class="testclass">我是test</span>
+    </div>
+</template>
+
+<script>
+</script>
+
+<style>
+    #test {
+          font-family: 'Avenir', Helvetica, Arial, sans-serif;
+          -webkit-font-smoothing: antialiased;
+          -moz-osx-font-smoothing: grayscale;
+          text-align: center;
+          color: #2c3e50;
+          margin-top: 160px;
+      }
+</style>
